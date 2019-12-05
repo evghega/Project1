4b1632a55cf352ca0eb57c2b7de9ecde64c63ad3
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2019-12-05 20:40:11 +0200

    change

--- projectOne.py
+++ projectOne.py
@@ -21,29 +21,28 @@ class WorkingStudent(Employee, Student):
         self.same_institude = same_institute
 
 
-def main():
-    size = input('Enter size for arr')
-    arr = []
-    for i in range(size):
-        type = input("Enter type of person(Studen , Employee,WorkingStudent):")
-        if type == "student":
-            name = input("Enter name:")
-            id = input("Enter ID:")
-            age = input("Enter age:")
-            avarage = input("Enter avareage:")
-            institute = input("Enter intitute")
-            arr[i] = Student(name, id, age, avarage, institute)
-        if type == "Employee":
-            name = input("Enter name:")
-            id = input("Enter ID:")
-            age = input("Enter age:")
-            salary = input("Enter salary")
-            arr[i] = Employee(name, id, age, salary)
-        if type == "WorkingStudent ":
-            name = input("Enter name:")
-            id = input("Enter ID:")
-            age = input("Enter age:")
-            same_institute = input("Enter same_institute")
-            arr[i] = WorkingStudent(name, id, age, same_institute)
+size = input("Enter size for arr'")
+arr = []
+for i in range(size):
+     type = input("Enter type of person(Studen , Employee,WorkingStudent):")
+    if type == "student":
+        name = input("Enter name:")
+         id = input("Enter ID:")
+         age = input("Enter age:")
+         avarage = input("Enter avareage:")
+         institute = input("Enter intitute")
+         arr[i] = Student(name, id, age, avarage, institute)
+    if type == "Employee":
+       name = input("Enter name:")
+       id = input("Enter ID:")
+       age = input("Enter age:")
+       salary = input("Enter salary")
+       arr[i] = Employee(name, id, age, salary)
+    if type == "WorkingStudent ":
+        name = input("Enter name:")
+        id = input("Enter ID:")
+        age = input("Enter age:")
+        same_institute = input("Enter same_institute")
+        arr[i] = WorkingStudent(name, id, age, same_institute)
 
 
