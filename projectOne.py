e4ada1c28c46d718ed3ae934e619795b6528e5db
Snir Ben Yossef <snirben@gmail.com>
2019-12-05 19:37:42 +0200

    working

--- projectOne.py
+++ projectOne.py
@@ -3,4 +3,8 @@ class Person:
     self.name = name
     self.id = id
     self.age = age
-    
+
+
+class WorkingStudent(Employee,Student):
+  def __init__(self, same_institute):
+    self.same_institude= same_institute
