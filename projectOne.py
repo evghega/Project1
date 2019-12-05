467203a895be3ddcf4f4dc892f53364bf18979a2
evghega <evghega@ac.sce.ac.il>
2019-12-05 19:59:01 +0200

    Student

--- projectOne.py
+++ projectOne.py
@@ -3,18 +3,17 @@ class Person:
     self.name = name
     self.id = id
     self.age = age
-<<<<<<< HEAD
-    
 
+
+    
 class Student(Person):
 	def _init_(self, avrg, ins):
 		self.average=avrg
 		self.institute=ins
-		
-=======
+
+
 
 
 class WorkingStudent(Employee,Student):
   def __init__(self, same_institute):
     self.same_institude= same_institute
->>>>>>> e4ada1c28c46d718ed3ae934e619795b6528e5db
