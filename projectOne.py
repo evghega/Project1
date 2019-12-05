04374acf05ee71e8d834e650229c71f39620f629
TzlilLevi <57330791+TzlilLevi@users.noreply.github.com>
2019-12-05 19:57:32 +0200

    main

--- projectOne.py
+++ projectOne.py
@@ -3,4 +3,29 @@ class Person:
     self.name = name
     self.id = id
     self.age = age
-    
+
+
+def main():
+    size = input('Enter size for arr')
+    arr=[]
+    for i in range(size):
+        type = input("Enter type of person(Studen , Employee,WorkingStudent):")
+        if type == "student":
+            name = input("Enter name:")
+            id = input ("Enter ID:")
+            age = input("Enter age:")
+            avarage = input("Enter avareage:")
+            institute = input("Enter intitute")
+            arr[i]=Studen(name,id,age,avarage,institute)
+        if type == "Employee":
+            name = input("Enter name:")
+            id = input("Enter ID:")
+            age = input("Enter age:")
+            salary = input("Enter salary")
+            arr[i] = Studen(name, id, age,salary)
+        if type == "WorkingStudent ":
+            name = input("Enter name:")
+            id = input("Enter ID:")
+            age = input("Enter age:")
+            same_institute = input ("Enter same_institute")
+            arr[i] = Studen(name, id, age,same_institute)
